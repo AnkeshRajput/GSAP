9cfa6bec2330608c1980d27cc648f9ba403cca3d
AnkeshRajput <singhankesh446@gmail.com>
2025-06-23 14:41:52 +0530

    Implemented Contact information

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
 import About from "./components/About";
 import Art from "./components/Art";
+import Menu from "./components/Menu";
+import Contact from "./components/Contact";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -14,11 +16,13 @@ const App = () => {
 
   return (
     <main>
-      <Navbar></Navbar>
-      <Hero></Hero>
-      <Cocktails></Cocktails>
-      <About></About>
-      <Art></Art>
+      <Navbar/>
+      <Hero/>
+      <Cocktails/>
+      <About/>
+      <Art/>
+      <Menu/>
+      <Contact/>
     </main>
   );
 };
